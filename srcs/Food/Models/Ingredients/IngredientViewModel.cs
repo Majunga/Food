ae344f0bcc37fd58f479c53dc9b2f02cf9387913
Ethan McWilliams <ethan.mcwilliams@majunga.co.uk>
2019-10-22 13:02:40 +0100

    Split out viewmodels and removed quantity from Ingredient view

--- srcs/Food/Models/Ingredients/IngredientViewModel.cs
+++ srcs/Food/Models/Ingredients/IngredientViewModel.cs
@@ -1,27 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Common.DataTypes;
-using Common.EnumDataTypes;
-
-namespace Food.Models.Ingredients
+﻿namespace Food.Models.Ingredients
 {
     public class IngredientViewModel
     {
         public int? Id { get; set; }
         public string Name { get; set; }
-        public IngredientQuantityViewModel IngredientQuantity { get; set; }
-    }
-
-    public class IngredientQuantityViewModel
-    {
-        public decimal Quantity { get; set; }
-        public QuantityType QuantityType { get; set; }
-
-        public override string ToString()
-        {
-            return $"{Quantity}{QuantityType}";
-        }
     }
 }
---
+++ srcs/Food/Models/Shared/IngredientQuantityViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Common.EnumDataTypes;
+
+namespace Food.Models.Ingredients
+{
+    public class IngredientQuantityViewModel
+    {
+        public decimal Quantity { get; set; }
+        public QuantityType QuantityType { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Quantity}{QuantityType}";
+        }
+    }
+}

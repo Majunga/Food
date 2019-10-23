5a31a2bfa7b44ac85b2dd24f0cd2ab1f6f614010
Ethan McWilliams <ethan.mcwilliams@majunga.co.uk>
2019-10-23 22:39:32 +0100

    Added Delete command, couldn't really test since dotnet is now locking out the dll's with the dotnet host process

---
+++ srcs/Food.Service/IngredientHandlers/Commands/DeleteIngredientCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Common.Conversion;
+using Food.Dal;
+using Food.Dal.Models;
+using Food.IService.IngredientHandlers.Commands;
+
+namespace Food.Service.IngredientHandlers.Commands
+{
+    public class DeleteIngredientCommandHandler : CommandHandlerBase<DeleteIngredientCommand>
+    {
+        public DeleteIngredientCommandHandler(IUnitOfWork unitOfWork, IConversionService converter) : base(unitOfWork, converter)
+        {
+        }
+
+        public override void Handle(DeleteIngredientCommand command)
+        {
+            this.UnitOfWork.IngredientRepository.Delete(command.IngredientId);
+            this.UnitOfWork.SaveChanges();
+        }
+    }
+}
